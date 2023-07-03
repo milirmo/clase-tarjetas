b1c5cd5e7fad25385d0c9247e18b60ddfc764f23
milirmo <110854465+milirmo@users.noreply.github.com>
2023-07-03 17:00:44 -0300

    Add files via upload

--- clase- tarjetas/romero.js
+++ clase- tarjetas/romero.js
@@ -1,13 +1,59 @@
-function elegirdivs() {
-    let selector=document.getElementById("seleccion");
-    let opcion=parseInt(selector.value);
+function Elegirdivs() {
+    let selector = document.getElementById("seleccion");
+    let opcion = parseInt(selector.value);
 
-    let divs=document.querySelectorAll('.tarjeta');
-    for (i=0; i<divs.length; i++) {
-        if(i<opcion) {
-        divs [i].classList.remove("hidden");
+    let divs = document.querySelectorAll('.tarjeta');
+    for (i = 0; i < divs.length; i++) {
+        if (i < opcion) {
+            divs[i].classList.remove("hidden");
         } else {
             divs[i].classList.add("hidden");
+        }
+    }
+}
+
+function Elegirtipo() {
+    let selectortipo = document.getElementById("seleccion2");
+    let opcion = parseInt(selectortipo.value);
+
+    let razachica = document.querySelectorAll('.chico');
+    let razamediana = document.querySelectorAll('.mediano');
+    let razagrande = document.querySelectorAll('.grande');
+
+    if (opcion === 3) {
+        for (i = 0; i < razachica.length; i++) {
+            razachica[i].classList.remove("hidden");
+        }
+        for (c=0; c<razamediana.length; c++) {
+            razamediana[c].classList.add("hidden");
+        }
+        for (k=0; k<razagrande.length; k++) {
+            razagrande[k].classList.add("hidden");
+        }
+    } else {
+        if (opcion === 2) {
+            for (i = 0; i < razamediana.length; i++) {
+                razamediana[i].classList.remove("hidden");
+            }
+            for (k=0; k<razagrande.length; k++) {
+                razagrande[k].classList.add("hidden");
+            }
+            for (c=0; c<razachica.length; c++) {
+                razachica[c].classList.add("hidden");
+            }
+
+        } else {
+            if (opcion === 1) { 
+                for (i = 0; i < razagrande.length; i++) {
+                    razagrande[i].classList.remove("hidden");
+                }
+                for (c=0; c<razachica.length; c++) {
+                    razachica[c].classList.add("hidden");
+                }
+                for (k=0; k<razamediana.length; k++) {
+                    razamediana[k].classList.add("hidden");
+                }
+            }
         }
     }
 }
